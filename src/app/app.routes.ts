fa47a7d8f472d50f94aba47c7fd17ab6dd8c5237
Carlos Amaral <carloshsamaral@gmail.com>
2024-05-20 08:20:09 -0300

    criaçao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './componenets/pages/login/login.component';
+import { AdminComponent } from './componenets/pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pages/login',
+    component: LoginComponent},
+
+  {
+    path: 'pages/admin',
+    component: AdminComponent},
+  {
+    path: '',
+    pathMatch : 'full',
+    redirectTo: '/pages/login'
+  }
+];
